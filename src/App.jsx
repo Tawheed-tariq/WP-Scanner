2d313d8f2d533d3b6d8cb3b3a6fdbdf9c15fb50a
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-02-29 19:33:17 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,31 @@ import Home from './pages/Home/Home'
 import './App.css'
 import SignUp from './pages/signup/SignUp'
 import Login from './pages/login/Login'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/signup",
+      element: <SignUp/>,
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+  ]);
 
   return (
     <>
-      {/* <Home/> */}
-      <Login/>
+    <RouterProvider router={router} />
     </>
   )
 }
