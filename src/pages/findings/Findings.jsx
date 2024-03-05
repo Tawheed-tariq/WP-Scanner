24d686ce7122966e7938e5ac05e2ee3f296a3383
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-03-05 21:19:25 +0530

    changed folder

--- src/pages/findings/Findings.jsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "../../components/DashboardLayout";
-
-export default function Findings(){
-    return(
-        <DashboardLayout title={`Findings`}>
-            
-        </DashboardLayout>
-    )
-}
--- src/pages/report/Report.jsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "../../components/DashboardLayout";
-
-export default function Report(){
-    return(
-        <DashboardLayout title={`Report`}>
-            
-        </DashboardLayout>
-    )
-}
