cd0cb9206b85ec50f040db8806d66fd7a6ff9359
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-03-07 12:02:57 +0530

    minor changes

--- frontend/src/constants/finding.js
+++ frontend/src/constants/finding.js
@@ -27,5 +27,46 @@ export const resultToSend = [
                 ["Subdomain3.com", "192.168.78", "Google"],
             ]
         }
-    }
+    },
+    {
+        id: 3,
+        title : 'Vulnerability Scan results',
+        img : 'vulnerability.svg',
+        res : '8 vulnerabilities found',
+        target : 'http://casetcollege.in',
+        data : {
+            headings : ["Plugin Name", "vulnerabilities"],
+            dataRows : [
+                [
+                    "elementor", 
+                    `Elementor Website Builder < 3.16.5 - Authenticated (Contributor+) Stored Cross-Site Scripting via get_inline_svg()
+                    Version: 3.16.5
+                    
+                    Elementor Website Builder < 3.16.5 - Missing Authorization to Arbitrary Attachment Read
+                    Version : 3.16.5`
+                ],
+                [
+                    "element-skit-lite", 
+                    `Elements kit Elementor addons < 2.9.2 - Missing Authorization
+                    Version: 2.9.2
+
+                    ElementsKit Lite < 3.0.4 - Unauthenticated Sensitive Information Exposure
+                    Version: 3.0.4`
+                ],
+            ]
+        }
+    },
+    {
+        id: 4,
+        res : 'Genral Info',
+        target : '127.0.0.1',
+        data : {
+            headings : ["Title", "Country", "HTTP Server", "IP", "Email", "Bootstrap Version", "Jquery Version"],
+            dataRows : [
+                [' The Lead Generation Form Builder','UNITED STATES', 'Apache', '162.240.76.5', 'info@leadgenapp.io', '4.0.0,6.3.3', '1.11.0,3.6.0,3.7.0']
+            ]
+        }
+    },
 ]
+
+
