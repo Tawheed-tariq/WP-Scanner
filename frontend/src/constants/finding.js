0fe4c5d1f3aac7851fa51bdaa0e77ac68c6466f6
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-03-09 10:10:05 +0530

    changes in output

--- frontend/src/constants/finding.js
+++ frontend/src/constants/finding.js
@@ -3,69 +3,90 @@ export const resultToSend = [
         id: 1,
         title : 'Active Scan Results',
         img : 'active.svg',
-        res : '4 Ports Found',
         target : 'http://casetcollege.in',
-        data : {
-            headings : ["Port", "Protocol", "Satatus", "Service"],
-            dataRows : [
-                ["8080", "tcp", "Open", "Apache"],
-                ["81", "tcp","Open", "Miscrosoft"],
-            ]
-        }
+        response :[
+            {
+                res : '14 Ports Found',
+                data : {
+                    headings : ["Port", "Protocol", "Status", "Service", "Version"],
+                    dataRows : [
+                        [21, 'tcp', 'open', 'ftp', 'Pure-FTPd'],
+                        [22, 'tcp', 'open', 'ssh', 'OpenSSH 7.4 (protocol 2.0)'],
+                        [26, 'tcp', 'open', 'smtp', 'Exim smtpd 4.96.2'],
+                        [80, 'tcp', 'open', 'http', 'Apache httpd'],
+                        [110, 'tcp', 'open', 'pop3', 'Dovecot pop3d'],
+                        [143, 'tcp', 'open', 'imap', 'Dovecot imapd'],
+                        [443, 'tcp', 'open', 'ssl/http', 'Apache httpd'],
+                        [554, 'tcp', 'open', 'rtsp', 'None'],
+                        [587, 'tcp', 'open', 'smtp', 'Exim smtpd 4.96.2'],
+                        [993, 'tcp', 'open', 'imaps', 'None'],
+                        [995, 'tcp', 'open', 'pop3s', 'None'],
+                        [1723, 'tcp', 'open', 'tcpwrapped', 'None'],
+                        [2222, 'tcp', 'open', 'ssh', 'OpenSSH 7.4 (protocol 2.0)'],
+                        [3306, 'tcp', 'open', 'mysql', 'MySQL 5.7.23-23'],
+                    ]
+                }
+            },
+            {
+                res : 'Genral Info',
+                data : {
+                    headings : ["Title", "Country", "HTTP Server", "IP", "Email", "Bootstrap Version", "Jquery Version"],
+                    dataRows : [
+                        [' The Lead Generation Form Builder','UNITED STATES', 'Apache', '162.240.76.5', 'info@leadgenapp.io', '4.0.0,6.3.3', '1.11.0,3.6.0,3.7.0']
+                    ]
+                }
+            },
+            {
+                res : '3 subdomains found',
+                data : {
+                    headings : ["Sub Domain", "IP Address", "NetName"],
+                    dataRows : [
+                        ["Subdomain1.com", "127.0.0.1", "Miscrosoft"],
+                        ["Subdomain2.com", "192.168.91", "Miscrosoft"],
+                        ["Subdomain3.com", "192.168.78", "Google"],
+                    ]
+                }
+            }
+        ]
     },
     {
         id: 2,
         title : 'Sub Domain Finder results',
         img : 'subdom.svg',
-        res : '16 subdomains found',
         target : '127.0.0.1',
-        data : {
-            headings : ["Sub Domain", "IP Address", "NetName"],
-            dataRows : [
-                ["Subdomain1.com", "127.0.0.1", "Miscrosoft"],
-                ["Subdomain2.com", "192.168.91", "Miscrosoft"],
-                ["Subdomain3.com", "192.168.78", "Google"],
-            ]
-        }
+        response : []
     },
     {
         id: 3,
         title : 'Vulnerability Scan results',
         img : 'vulnerability.svg',
-        res : '8 vulnerabilities found',
         target : 'http://casetcollege.in',
-        data : {
-            headings : ["Plugin Name", "vulnerabilities"],
-            dataRows : [
-                [
-                    "elementor", 
-                    `Elementor Website Builder < 3.16.5 - Authenticated (Contributor+) Stored Cross-Site Scripting via get_inline_svg()
-                    Version: 3.16.5
-                    
-                    Elementor Website Builder < 3.16.5 - Missing Authorization to Arbitrary Attachment Read
-                    Version : 3.16.5`
-                ],
-                [
-                    "element-skit-lite", 
-                    `Elements kit Elementor addons < 2.9.2 - Missing Authorization
-                    Version: 2.9.2
+        response : [
+            {
+                res : '8 vulnerabilities found',
+                data : {
+                    headings : ["Plugin Name", "vulnerabilities"],
+                    dataRows : [
+                        [
+                            "elementor", 
+                            `Elementor Website Builder < 3.16.5 - Authenticated (Contributor+) Stored Cross-Site Scripting via get_inline_svg()
+                            Version: 3.16.5
+                            
+                            Elementor Website Builder < 3.16.5 - Missing Authorization to Arbitrary Attachment Read
+                            Version : 3.16.5`
+                        ],
+                        [
+                            "element-skit-lite", 
+                            `Elements kit Elementor addons < 2.9.2 - Missing Authorization
+                            Version: 2.9.2
 
-                    ElementsKit Lite < 3.0.4 - Unauthenticated Sensitive Information Exposure
-                    Version: 3.0.4`
-                ],
-            ]
-        }
-    },
-    {
-        id: 4,
-        res : 'Genral Info',
-        target : '127.0.0.1',
-        data : {
-            headings : ["Title", "Country", "HTTP Server", "IP", "Email", "Bootstrap Version", "Jquery Version"],
-            dataRows : [
-                [' The Lead Generation Form Builder','UNITED STATES', 'Apache', '162.240.76.5', 'info@leadgenapp.io', '4.0.0,6.3.3', '1.11.0,3.6.0,3.7.0']
-            ]
-        }
+                            ElementsKit Lite < 3.0.4 - Unauthenticated Sensitive Information Exposure
+                            Version: 3.0.4`
+                        ],
+                    ]
+                }
+            }
+        ]
     },
 ]
 
--- frontend/src/pages/findings/Findings.jsx
+++ frontend/src/pages/findings/Findings.jsx
@@ -11,9 +11,8 @@ export default function Findings(){
                         key={result.id}
                         title={result.title}
                         img={result.img}
-                        res={result.res}
                         target={result.target}
-                        data={result.data}
+                        response={result.response}
                     />
                 ))
             }
@@ -21,49 +20,53 @@ export default function Findings(){
     )
 }
 
-const Result = ({title, img, res, target, data}) => {
+const Result = ({title, img, target, response}) => {
     return(
         <div className={`mx-[20px] my-[30px]`}>
             <div className={`flex gap-[10px] items-center`}>
                 <img src={img} alt="" />
                 <p className={`font-medium text-[24px]`}>{title}</p>
             </div>
-            <div className={`mt-[20px]`}>
-                <div className={`flex gap-[10px] items-center`}>
-                    <GiGalaxy color={`#226F78`} size={`25`}/>
-                    <p className={`font-medium text-[22px]`}>{res}</p>
-                </div>
-                <div className={`mt-[10px]`}>
-                    <div className="flex w-full items-center border-text border-[1px] border-b-0 py-[7px] px-[15px] gap-[10px] bg-primary ">
-                        <HiComputerDesktop color={`#226F78`} size={`25`}/>
-                        <p className={`font-medium text-[20px]`}>{target}</p>
-                    </div>
-                    <table className={`w-full border-collapse Table text-text border-text table-auto tab border-[1px]`}>
-                        <thead className="bg-primary">
-                            <tr>
-                                {
-                                    data.headings.map((ele) => (
-                                        <td>{ele}</td>
-                                    ))
-                                }
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                data.dataRows.map((row) => (
+            {
+                response.map((item) => (
+                    <div className={`mt-[20px]`}>
+                        <div className={`flex gap-[10px] items-center`}>
+                            <GiGalaxy color={`#226F78`} size={`25`}/>
+                            <p className={`font-medium text-[22px]`}>{item.res}</p>
+                        </div>
+                        <div className={`mt-[10px]`}>
+                            <div className="flex w-full items-center border-text border-[1px] border-b-0 py-[7px] px-[15px] gap-[10px] bg-primary ">
+                                <HiComputerDesktop color={`#226F78`} size={`25`}/>
+                                <p className={`font-medium text-[20px]`}>{target}</p>
+                            </div>
+                            <table className={`w-full border-collapse Table text-text border-text table-auto tab border-[1px]`}>
+                                <thead className="bg-primary">
                                     <tr>
                                         {
-                                            row.map((data, index) => (
-                                                <td className={index== 0 ? 'font-semibold text-accent text-[22px]': ''}>{data}</td>
+                                            item.data.headings.map((ele) => (
+                                                <td>{ele}</td>
                                             ))
                                         }
                                     </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                                </thead>
+                                <tbody>
+                                    {
+                                        item.data.dataRows.map((row) => (
+                                            <tr>
+                                                {
+                                                    row.map((data, index) => (
+                                                        <td className={index== 0 ? 'font-semibold text-accent text-[22px]': ''}>{data}</td>
+                                                    ))
+                                                }
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
 
     )
