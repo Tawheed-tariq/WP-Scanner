242bfd2a3340b67356d7c72256fe0f1951f1b699
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-03-12 22:30:07 +0530

    responsive changes

--- frontend/src/pages/Home/components/Navbar.jsx
+++ frontend/src/pages/Home/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export default function NavBar(){
     return(
         <nav className={`w-full h-[70px] bg-home-secondary fixed z-[999] backdrop-blur-xl px-[20px] md:px-[100px] lg:px-[200px] flex items-center justify-between`}>
-            <h1 className={`text-[32px] text-txt font-bold`}>WPScans</h1>
-            <div className={`flex items-center gap-[20px]`}>
+            <h1 className={`text-[20px] sm:text-[28px] md:text-[32px] text-txt font-bold`}>WPScans</h1>
+            <div className={`sm:flex hidden items-center gap-[20px]`}>
                 
                 {
                     navLinks.map((ele) => (
@@ -19,9 +19,9 @@ export default function NavBar(){
                 
             </div>
             <Link to={`/login`}>
-                <div className={`w-[150px] h-[50px] cursor-pointer bg-home-accent rounded-full flex items-center justify-between px-[30px]`}>
-                    <p className={`text-xl text-text font-medium`}>Login</p>
-                    <img className={`w-[20px] h-[20px] `} src="user.png" alt="" />
+                <div className={`w-[110px] md:w-[150px] h-[40px] md:h-[50px] cursor-pointer bg-home-accent rounded-full flex items-center justify-between px-[20px] md:px-[30px]`}>
+                    <p className={`text-[16px] sm:text-[18px] md:text-[20px] text-text font-medium`}>Login</p>
+                    <img className={`w-[18px] h-[18px] md:w-[20px] md:h-[20px] `} src="user.png" alt="" />
                 </div>
             </Link>
         </nav>
--- frontend/src/pages/Home/components/Working.jsx
+++ frontend/src/pages/Home/components/Working.jsx
@@ -1,17 +1,17 @@
 import { elements } from "../../../constants"
 export default function Working(){
     return(
-        <div id="work" className={`bg-home-bg w-full working px-[20px] md:px-[100px] lg:px-[200px] text-txt flex flex-col items-center justify-center pt-[200px]`}>
-            <div className={`w-[1200px]`}>
-                <h1 className="text-[58px] mx-auto font-extrabold w-[1000px]">How does the Website Vulnerability scanner work</h1>
+        <div id="work" className={`bg-home-bg w-full working px-[20px] md:px-[100px] lg:px-[200px] text-txt flex flex-col items-center justify-center md:pt-[200px]`}>
+            <div className={`xl:w-[1200px]`}>
+                <h1 className="text-[34px] sm:text-[44px] md:text-[58px] mx-auto font-extrabold xl:w-[1000px]">How does the Website Vulnerability scanner work</h1>
 
-                <p className={`text-[20px] mt-[10px]`}>
+                <p className={`text-[14px] sm:text-[16px] md:text-[18px] mt-[10px]`}>
                     The Website Vulnerability Scanner is a DAST (Dynamic Application  Security Testing) 
                     tool which tries to discover vulnerabilities like XSS,  SQL injection, HTTP Prototype 
                     Pollution, Directory Traversal, and more  in running web applications.
                 </p>
 
-                <p className={`text-[20px] my-[25px]`}>
+                <p className={`text-[14px] sm:text-[16px] md:text-[18px] my-[10px] md:my-[25px]`}>
                     The scanner interacts with the target application by sending numerous 
                     HTTP requests with specific payloads. If the application is vulnerable,  
                     these payloads will determine the code to behave abnormally, informing  
@@ -38,11 +38,11 @@ export default function Working(){
 const Item = ({title, url, desc}) => {
     return(
         <div className={`py-[20px]  scan border-l-[5px] border-t-[5px] border-home-primary px-[30px] rounded-3xl bg-home-secondary max-w-[600px]`}>
-            <div className="flex items-center gap-[30px] mb-[20px]">
-                <img className={`w-[100px] h-[100px]`} src={url} alt="" />
-                <h1 className="text-[40px] font-bold ">{title}</h1>
+            <div className="flex items-center gap-[10px] md:gap-[30px] mb-[10px] md:mb-[20px]">
+                <img className={`w-[30px] h-[30px] md:w-[100px] md:h-[100px]`} src={url} alt="" />
+                <h1 className="text-[24px] md:text-[40px] font-bold ">{title}</h1>
             </div>
-            <p className={`text-[16px]`}>
+            <p className={`text-[12px] md:text-[16px]`}>
                 {desc}
             </p>
         </div>
