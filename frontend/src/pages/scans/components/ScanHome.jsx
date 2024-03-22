d00c2fa481060ffa266e3028e7f46351e170c654
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-03-22 22:23:57 +0530

    commit

--- frontend/src/pages/scans/components/ScanHome.jsx
+++ frontend/src/pages/scans/components/ScanHome.jsx
@@ -2,6 +2,22 @@ import DashboardLayout from "../../../components/DashboardLayout";
 import { TbActivityHeartbeat } from "react-icons/tb";
 import { Link } from "react-router-dom";
 export default function ScanHome(){
+    const headings = ["Name", "schedule", "Last Scanned"]
+
+    const rows = [
+        {
+            name : "1st scan",
+            schedule : "on demand",
+            status : "running",
+            time : "today 12:00pm"
+        },
+        {
+            name : "2nd scan",
+            schedule : "on demand",
+            status : "saved",
+            time : "tommorow 12:00pm"
+        }
+    ]
     return(
         <DashboardLayout title={`Scans`}>
             {/* New Scan  */}
@@ -15,35 +31,21 @@ export default function ScanHome(){
             </div>
 
             {/* Scan results table*/}
-            <div className={`w-full py-[5px] flex flex-col gap-[15px] px-[20px]`}>
-                <div className={`w-full flex justify-between  px-[20px] py-[10px] rounded-2xl boxShadow bg-secondary-50`}>
-                    <p className={`text-text font-medium text-[16px] md:text-[22px]`}>Scans</p>
-                    <p className={`text-text font-medium text-[16px] md:text-[22px]`}>Status</p>
-                    <p className={`text-text font-medium text-[16px] md:text-[22px]`}>Target</p>
-                    <p className={`text-text font-medium text-[16px] md:text-[22px]`}>Date</p>
-                </div>
-            {/* data rows */}
-                <div className={`w-full flex gap-[10px] justify-between border-text border-[1px] px-[20px] py-[10px]`}>
-                    <p className={`text-accent text-[14px] md:text-[20px]`}>Active Scan</p>
-                    <p className={`text-text text-[14px] md:text-[20px]`}>Completed</p>
-                    <p className={`text-accent text-[14px] md:text-[20px]`}>127.0.0.1</p>
-                    <p className={`text-text text-[14px] md:text-[20px]`}>24-01-2024</p>
-                </div>
+            <table className={`w-full border-collapse Table text-text border-text table-auto tab border-[1px]`}>
+                <thead className="bg-primary">
+                    <tr>
+                        {
+                            headings.map((ele, eleIndex) => (
+                                <td key={eleIndex}>{ele}</td>
+                            ))
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
 
-                <div className={`w-full flex gap-[10px] justify-between border-text border-[1px] px-[20px] py-[10px]`}>
-                    <p className={`text-accent text-[14px] md:text-[20px]`}>Active Scan</p>
-                    <p className={`text-text text-[14px] md:text-[20px]`}>Completed</p>
-                    <p className={`text-accent text-[14px] md:text-[20px]`}>127.0.0.1</p>
-                    <p className={`text-text text-[14px] md:text-[20px]`}>24-01-2024</p>
-                </div>
-
-                <div className={`w-full flex gap-[10px] justify-between border-text border-[1px] px-[20px] py-[10px]`}>
-                    <p className={`text-accent text-[14px] md:text-[20px]`}>Active Scan</p>
-                    <p className={`text-text text-[14px] md:text-[20px]`}>Completed</p>
-                    <p className={`text-accent text-[14px] md:text-[20px]`}>127.0.0.1</p>
-                    <p className={`text-text text-[14px] md:text-[20px]`}>24-01-2024</p>
-                </div>
-            </div>
         </DashboardLayout>
     )
 }
